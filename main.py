48bb357be5f246a27db405966bd8f93fb59aa7f9
Vova <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>
2022-07-18 15:07:37 +0300

    rename function correctly

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ async def fetch_users():
 @app.post('/api/v1/users')
 async def register_user(user: User):
     db.append(user)
-    return {"id": user.id}\
+    return {"id": user.id}
 
 
 @app.delete('/api/v1/users/{user_id}')
-async def register_user(user_id: UUID):
+async def delete_user(user_id: UUID):
     for user in db:
         if user.id == user_id:
             db.remove(user)
