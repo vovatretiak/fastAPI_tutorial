e826c6b38bad7b8c84d1d5f47afd061144579f4a
Vova <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>
2022-07-18 14:42:53 +0300

    create function to add users

--- main.py
+++ main.py
@@ -32,3 +32,9 @@ async def root():
 @app.get('/api/v1/users')
 async def fetch_users():
     return db
+
+
+@app.post('/api/v1/users')
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
